d2488ae35edc37c5549486e9c70f9c1830281d76
worapon <worapon.p@hobbiz.co.th>
2021-03-06 17:16:10 +0700

    env

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,17 +1,17 @@
 <template>
   <div>
-    <h1>Hello world!</h1>
+    <h1>contact</h1>
     <button @click="redirect">redirect</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Home",
+  name: "Contact",
   methods: {
     redirect() {
       this.$router.push({
-        path: "/contact",
+        path: "/",
       });
     },
   },
